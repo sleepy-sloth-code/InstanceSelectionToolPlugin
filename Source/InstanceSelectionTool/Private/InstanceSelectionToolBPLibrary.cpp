326017e2e904c31af015b9f71e078ca7674b3ed0
sleepy-sloth-code <benwilson003@gmail.com>
2025-12-04 16:25:41 +0000

    Uploading tool files
    Adding the Content, Resources, Source, and .uplugin files to the repository

---
+++ Source/InstanceSelectionTool/Private/InstanceSelectionToolBPLibrary.cpp
@@ -0,0 +1,54 @@
+﻿// References the Classes containing level instance functions
+#include "LevelInstance/LevelInstanceActor.h"
+#include "LevelInstance/LevelInstanceSubsystem.h"
+
+// References to Plugin
+#include "InstanceSelectionToolBPLibrary.h"
+#include "InstanceSelectionTool.h"
+
+UInstanceSelectionToolBPLibrary::UInstanceSelectionToolBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+/*
+ * Test Function
+float UInstanceSelectionToolBPLibrary::InstanceSelectionToolSampleFunction(float Param)
+{
+	return -1;
+}
+*/
+
+// Create Level Instance, with an option for creating a Packed Level Actor also
+ALevelInstance* UInstanceSelectionToolBPLibrary::CreateLevelInstanceFromArray(
+	UObject* WorldContextObject,
+	const TArray<AActor*>& Actors,
+	const bool bPackedLevelActor //Default to false
+	)
+{	
+	//Resolve World argument, if no World Context is found return null
+	UWorld* const World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	if (World == nullptr)
+	{
+		return nullptr;
+	}
+	
+	// Resolve Level Instance Subsystem, if the subsystem can't be accessed then returns null and logs the error
+	ULevelInstanceSubsystem* const LevelInstanceSubsystem = World->GetSubsystem<ULevelInstanceSubsystem>();
+	if (LevelInstanceSubsystem == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%hs: Could not call the Level Instance Subsystem"), __FUNCTION__);
+		return nullptr;
+	}
+	
+	// Create Level Instance using Actors array and Params we resolved
+	// Depending on Packed Level Actor option will either create with or without
+	FNewLevelInstanceParams Params;
+	Params.Type = bPackedLevelActor ? ELevelInstanceCreationType::PackedLevelActor : ELevelInstanceCreationType::LevelInstance;
+	ILevelInstanceInterface* const LevelInstanceInterface = LevelInstanceSubsystem->CreateLevelInstanceFrom(Actors, Params);
+	
+	// Cast the result to a Level Instance Actor and return
+	ALevelInstance* const LevelInstanceActor = Cast<ALevelInstance>(LevelInstanceInterface);
+	return LevelInstanceActor;
+}
+
---
+++ Source/InstanceSelectionTool/Public/InstanceSelectionToolBPLibrary.h
@@ -0,0 +1,30 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "InstanceSelectionToolBPLibrary.generated.h"
+
+// References the Classes containing level instance functions
+class ALevelInstance;
+
+UCLASS()
+class UInstanceSelectionToolBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+	
+	/*
+	 * Test Function
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Execute Sample function", Keywords = "InstanceSelectionTool sample test testing"), Category = "InstanceSelectionToolTesting")
+	static float InstanceSelectionToolSampleFunction(float Param);
+	*/
+	
+	// Create Level Instance, with an option for creating a Packed Level Actor also
+	// Takes in arguments: World Context, array of actors, create packed level actor?
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject", DefaultToSelf))
+	static ALevelInstance* CreateLevelInstanceFromArray(
+		UObject* WorldContextObject,
+		const TArray<AActor*>& Actors,
+		const bool bPackedLevelActor = false
+	);
+};
